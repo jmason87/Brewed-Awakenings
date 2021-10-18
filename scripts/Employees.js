65d58b02c9f6ea2f5fb7702b2e8744edec6b2eed
jmason87 <joseph.mason87@gmail.com>
2021-10-18 10:59:34 -0500

    initial commit

---
+++ scripts/Employees.js
@@ -0,0 +1,45 @@
+import { getEmployees } from "./database.js"
+import { getOrders } from "./database.js"
+import { Products } from "./Products.js"
+
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [, employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    
+                    const employeeOrders = orders.filter(
+                        (order) => { 
+                            if (order.employeeId === employee.id) {
+                                return true
+                            }
+                        }
+                    )
+                    window.alert(`${employee.name} sold ${employeeOrders.length} products`)
+                }
+            }
+        }
+    }
+)
+
+const employees = getEmployees()
+const orders = getOrders()
+
+export const Employees = () => {
+    let html = "<ul>"
+
+    for (const employee of employees) {
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
